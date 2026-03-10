fb0607dbf882ea2381a352aed75fe714127dd7ef
Isabelle Smith <123012188+isabelle-smith@users.noreply.github.com>
2026-03-10 14:43:55 -0400

    2026.03.10

--- R/iprint.R
+++ R/iprint.R
@@ -1,31 +1,35 @@
 
 ## Isabelle T. Smith, 2025.
 
+## 2025.08.27
+
 ########################################################################################################################
 
 
 #' Printing function for RMD chunks.
-#' 
+#'
 #' `iprint()` prints object(s) using [cat()], without spaces
 #' in-between objects, and with an optional return at the end.
 #' Ideal for labeling output in a chunk when working in RMD.
-#' 
+#'
 #' This function is not worth using over [cat()] if you are
 #' printing to the console (even if you don't want spaces).
-#' 
 #'
-#' @param ... R object(s)
+#'
+#' @param ... R object(s).
 #' @param newline logical. Whether or not to go to a
 #'  new line at the end of printing; defaults to `TRUE`.
 #'
 #' @return Nothing (actually an invisible `NULL`; see
 #'  [cat()] for details).
+#'
 #' @export
 #'
 #' @examples
 #' ## default (newline=TRUE)
 #' iprint(c("a","b","c"))
 #' iprint(c("x","y","z"))
+#'
 #' ## no return at the end
 #' iprint(c("a","b","c"), newline=FALSE)
 #' iprint(c("x","y","z"), newline=FALSE)
@@ -36,13 +40,13 @@
 
 
 iprint <- function(..., newline=TRUE) {
-  
+
   if (newline){
     cat(..., "\n", sep="")
   } else {
     cat(..., sep="")
   }
-  
+
 }
 
 
---
+++ R/izzy_html.R
@@ -0,0 +1,72 @@
+
+## modified: Isabelle T. Smith, 2026.
+## created:  Yihui Xie, J. J. Allaire, Garrett Grolemund, 2026.
+
+## 2026.03.10
+
+########################################################################################################################
+
+
+#' HTML Rendering for package `isabelletsmith`
+#'
+#' `izzy_html()` prepares an RMD document to be rendered into HTML.
+#' It gets CSS and HTML template files from the `isabelletsmith` package
+#' (<https://github.com/isabelle-smith/isabelletsmith>), then passes
+#' those and any additional args into [rmarkdown::html_document()].
+#'
+#'
+#' @param default string.
+#' @param toc logical. Whether or not to include table of contents.
+#' @param toc_depth integer. Number of heading levels to include in table of contents.
+#' @param ... other arguments to be passed to [rmarkdown::html_document()].
+#'
+#' @return R Markdown output format, to use with [rmarkdown::render()].
+#'
+#' @export
+#'
+#' @examples
+#' ## base case
+#' render("file.Rmd", izzy_html())
+#'
+#' ## changing TOC defaults
+#' render("file.Rmd", izzy_html(toc=FALSE, toc_depth=5))
+#'
+#' ## different template name
+#' render("file.Rmd", izzy_html(default="007-default"))
+#'
+#' ## additional argumentss
+#' render("file.Rmd", izzy_html(code_folding="hide"))
+
+
+
+# --------------------------------------
+
+
+izzy_html = function(default="default", toc=TRUE, toc_depth=4, ...) {
+
+
+  # locations of resource files in the package
+  css       = system.file("Izzy-Files/styles.css",
+                         package = "isabelletsmith")
+
+  template  = system.file(paste0("Izzy-Files/", default, ".html"),
+                         package = "isabelletsmith")
+
+
+
+  # call the base html_document function
+  rmarkdown::html_document(toc           = toc,
+                           toc_depth     = toc_depth,
+                           css           = css,
+                           template      = template,
+                           ...)
+
+
+}
+
+
+# --------------------------------------
+
+
+
+########################################################################################################################
